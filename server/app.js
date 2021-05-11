b816fa56b420036ad7a7756ea74364131e98eb4b
lucaspchartier <lucaspchartier@gmail.com>
2021-05-10 20:16:19 -0400

    chore(server/app.js): add graphiql property in middleware

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const schema = require("./schema/schema.js");
 const app = express();
 
 app.use("/graphql", graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000, () => {
