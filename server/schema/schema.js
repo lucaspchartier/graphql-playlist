75eea472242f5e36d7971b7c420c45a623a787f3
lucaspchartier <lucaspchartier@gmail.com>
2021-05-10 20:16:19 -0400

    chore(schema/schema.js): add RootQuery object

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,29 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
-const BookType = new GraphQLOBjectType({
+const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
-        id: { type: GraphQLString }
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
