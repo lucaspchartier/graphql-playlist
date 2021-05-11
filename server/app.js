b3f46ffa283123c071fdebb7660610ba639d98dd
lucaspchartier <lucaspchartier@gmail.com>
2021-05-10 20:16:19 -0400

    chore(app.js): assign Node API port to 4000

--- server/app.js
+++ server/app.js
@@ -1,2 +1,7 @@
-import express from 'express';
+const express = require("express");
 
+const app = express();
+
+app.listen(4000, () => {
+    console.log("Now listening to requests on port 4000");
+});
