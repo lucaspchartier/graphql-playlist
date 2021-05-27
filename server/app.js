086e5288883d08a77c4b2409ebfe6d9b4a0e140d
lucaspchartier <lucaspchartier@gmail.com>
2021-05-26 20:55:26 -0400

    chore(app.js): import DB_URI string for MongoDB Atlas into middleware to connect to database
    Also set useNewUrlParser and useUnifiedTechnology
    properties to true in mongoose.connect({}) function

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema.js");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to database");
+});
 
 app.use("/graphql", graphqlHTTP({
     schema,
