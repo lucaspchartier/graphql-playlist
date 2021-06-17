1c8f3ae770c0344bc9095c3be2883e0b954e9253
lucaspchartier <lucaspchartier@gmail.com>
2021-06-16 20:38:37 -0400

    chore(src/App.js): reformat render method in App.js component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div id="main">
-      <h1>Reading List</h1>
-    </div>
-  );
-}
+import React, { Component } from "react";
 
+class App extends Component {
+    render() {
+        return (
+            <div id="main">
+                <h1>Reading List</h1>
+            </div>
+        );
+    }
+}
+  
 export default App;
