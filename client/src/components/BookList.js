d5f88b6422ae13dcc6486a21a3963cf2c40020f1
lucaspchartier <lucaspchartier@gmail.com>
2021-06-17 19:44:04 -0400

    chore(src/components/BookList.js): import apollo-boost and react-apollo dependencies in BookList.js component.
    Also set up gql query to retrieve book.

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,15 @@
 import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
 
 class BookList extends Component {
     render() {
