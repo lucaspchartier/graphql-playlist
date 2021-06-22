0446991c5d1d563f9dacc99816bf8283eec3ec7e
lucaspchartier <lucaspchartier@gmail.com>
2021-06-21 20:14:18 -0400

    chore(AddBook.js): add author form field to addBook component

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -11,3 +11,29 @@ const getAuthorsQuery = gql`
     }
 `
 
+class AddBook extends Component {    
+    render() {
+        return (
+            <form id="add-book">
+                <div className="field">
+                    <label>Book name:</label>
+                    <input type="text" />
+                </div>
+
+                <div className="field">
+                    <label>Genre:</label>
+                    <input type="text" />
+                </div>
+
+                <div className="field">
+                    <label>Author:</label>
+                    <select>
+                        <option>Select author</option>
+                    </select>
+                </div>
+
+                <button>+</button>
+            </form>
+        );
+    }
+}
