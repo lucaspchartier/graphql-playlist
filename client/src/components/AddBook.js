839319c936d72b6f46c4fddb0ddb48ea5c34b321
lucaspchartier <lucaspchartier@gmail.com>
2021-06-27 20:19:01 -0400

    chore(components/AddBook.js): add refetchQueries property in submitForm function that refreshes the page after a new book has been submitted

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import flowright from 'lodash.flowright';
-import { getAuthorsQuery, addBookMutation } from "../queries/queries";
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from "../queries/queries";
 
 class AddBook extends Component {
     constructor(props) {
@@ -31,7 +31,8 @@ class AddBook extends Component {
                 name: this.state.name,
                 genre: this.state.genre,
                 authorId: this.state.authorId
-            }
+            },
+            refetchQueries: [{ query: getBooksQuery }],
         });
     }
 
