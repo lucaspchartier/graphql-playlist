6d00b018b77cc11d445cd5cd2eac46d653f2c7cb
lucaspchartier <lucaspchartier@gmail.com>
2021-06-28 21:21:02 -0400

    chore(BookDetails.js): create displayBooksDetails() function to display author of a specific book and all books by that author

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getBookQuery } from "../queries/queries";
+// import BookList from "./BookList";
 
 class BookDetails extends Component {
+    displayBookDetails() {
+        const { book } = this.props.data;
+        if (book) {
+            return (
+                <div>
+                    <h2>{book.name}</h2>
+                    <p>{book.genre}</p>
+                    <p>{book.author.name}</p>
+                    <p>All books by this author:</p>
+                    <ul className="other-books">
+                        {book.author.books.map(book => <li key={book.id}>{book.name}</li>)}
+                    </ul>
+                </div>
+            )
+        } else {
+            return (
+                <div>No book selected...</div>
+            )
+        }
+    }
+
     render() {
         return (
             <div id="book-details">
-                <p>Output book details here</p>
+                {this.displayBookDetails()}
             </div>
         );
     }
 }
 
-export default graphql(getBookQuery)(BookDetails);
+export default graphql(getBookQuery, {
+    options: (props) => {
+        return {
+            variables: {
+                id: props.bookId
+            }
+        }
+    }
+})(BookDetails);
